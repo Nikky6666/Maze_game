d9efc13a3e614d933e8ea2db5f0eff2a6c69a7b1
Nikky6666 <borbut.nika@mail.ru>
2020-02-06 11:13:01 +0300

    Updates

---
+++ precache-manifest.08be9942350b6908e27ad9cd045860fa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920a01f1fb8a5493481508d4d03e7ef1",
+    "url": "/Maze_game/index.html"
+  },
+  {
+    "revision": "5aa7d082da9bdc35c459",
+    "url": "/Maze_game/static/css/main.19fc5d53.chunk.css"
+  },
+  {
+    "revision": "8d69e55bc1fd36adfae6",
+    "url": "/Maze_game/static/js/2.fffc7c96.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Maze_game/static/js/2.fffc7c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa7d082da9bdc35c459",
+    "url": "/Maze_game/static/js/main.eb5a69b8.chunk.js"
+  },
+  {
+    "revision": "00d1d4ba4fe124a0ceb9",
+    "url": "/Maze_game/static/js/runtime-main.fad88b97.js"
+  },
+  {
+    "revision": "ff8c342d18514ee3928da6457bacf348",
+    "url": "/Maze_game/static/media/backgroundImage.ff8c342d.webp"
+  },
+  {
+    "revision": "cfdbafe37f7253f018b83eb689f02d2e",
+    "url": "/Maze_game/static/media/errorChoise.cfdbafe3.png"
+  },
+  {
+    "revision": "11ef9e701603f7c0385daadf02b0795e",
+    "url": "/Maze_game/static/media/rightChoise.11ef9e70.png"
+  },
+  {
+    "revision": "3bcbb1a8a5797d7833cc7e35da5e9fdb",
+    "url": "/Maze_game/static/media/start-line.3bcbb1a8.svg"
+  }
+]);
